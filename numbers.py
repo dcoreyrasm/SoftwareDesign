f0153e92c1a31365f432b0942025623ef29a6208
dcorey.rasmussen <dcorey.rasmussen@gmail.com>
2016-08-23 19:38:20 -0400

    uploading code samples for class

---
+++ numbers.py
@@ -0,0 +1,9 @@
+a = raw_input('Enter a number: ')
+b = raw_input('Enter another number: ')
+print(a + b)
+
+a = raw_input('Enter a number: ')
+b = raw_input('Enter another number: ')
+a = int(a)
+b = int(b)
+print(a + b)
